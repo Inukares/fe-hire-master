6aa0468fafe47dc4e5493d50b0b8b78109dcba1e
inukares <zelowski@gmail.com>
2020-05-17 19:22:13 +0100

    let d3-array calculate median for you

--- src/orderbook/Orderbook/computeFairPrice.js
+++ src/orderbook/Orderbook/computeFairPrice.js
@@ -1,9 +1,8 @@
 import { round } from '../../utils/round';
+import { median } from 'd3-array';
 
-// number of elements is always odd, with the current worker implementation, but below could be easily improved
-// to handle cases where number of elements are even
 export const computeFairPrice = ({ asks, bids }) => {
-  const middleAsks = asks[Math.floor((asks.length - 1) / 2)].price;
-  const middleBids = bids[Math.floor((bids.length - 1) / 2)].price;
-  return round((parseFloat(middleAsks) + parseFloat(middleBids)) / 2, 1);
+  const asksPrice = asks.map((ask) => ask.price);
+  const bidsPrice = bids.map((bid) => bid.price);
+  return round((median(asksPrice) + median(bidsPrice)) / 2, 1);
 };
